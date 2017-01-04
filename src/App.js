f2eefd02c4801d0bd1e9ef548b55396127b15bb9
paulsevere <pjo2110@columbia.edu>
2017-01-04 17:41:57 -0500

    Initializes everything

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {Editor, EditorState, RichUtils, ContentState} from 'draft-js';
+
+import logo from './logo.svg';
+import './App.css';
+
+class MyEditor extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editorState: EditorState.createWithContent(ContentState.createFromText(`var message = "Hello World"
+    + "with four spaces indentation"
+
+console.log(message);`))
+        };
+        // console.log(this.state.editorState)
+        this.onChange = (editorState) => this.setState({editorState});
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    }
+    handleKeyCommand(command) {
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+        return 'not-handled';
+    }
+    render() {
+        return (<Editor editorState={this.state.editorState} handleKeyCommand={this.handleKeyCommand} onChange={this.onChange}/>);
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <MyEditor/>
+            </div>
+        );
+    }
+}
+
+export default App;
